0b832d4eed03847bb794ff6048084b6e5aa07c7c
Anuzrella <60137921+Anusha0808@users.noreply.github.com>
2025-05-30 21:16:29 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Anuzrella's Garden",
+    pageTitleSuffix: " testiing? ",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
