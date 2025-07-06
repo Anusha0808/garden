5b4a6b9535b108218cb6726348dc461e27204c8c
Anusha0808 <anushagupta2004@gmail.com>
2025-07-06 19:23:44 +0530

    Quartz sync: Jul 6, 2025, 7:23 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,6 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.AfterBodyStars],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/Anusha0808",
